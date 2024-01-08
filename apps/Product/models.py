410a1c87dbf4fa2c742c2e27265b27766d91c88e
Pouya <pouya.fallahi80@gmail.com>
2024-01-08 11:47:45 +0330

    CREATE | models app

---
+++ apps/Product/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name_category = models.CharField(max_length=254, blank=True, null=True)
+
+    def __str__(self):
+        return self.name_category
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=254, blank=True, null=True)
+    datil = models.TextField()
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.company_name
+
+
+class Product(models.Model):
+    name_product = models.CharField(max_length=255, blank=True, null=True)
+    datil = models.TextField()
+    price = models.IntegerField()
+    many = models.IntegerField()
+    price = models.IntegerField()
+    categotory = models.ForeignKey('Categorey', on_delete=models.CASCADE)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name_product
+
