3a1f558201315a2be02fab7a6936949836c65a5e
pouyaFallahi <pouya.fallahi80@gmail.com>
2024-01-16 21:04:10 +0330

    UPDATE | add login, register, logout

--- apps/User/views.py
+++ apps/User/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from .forms import SignUpForm
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.views.generic import CreateView, View
 
-# Create your views here.
+
+class LogingView(LoginView):
+    template_name = 'User/login.html'
+    success_url = reverse_lazy('')
+
+
+count_of_logout = {}
+
+
+class LogOutView(View):
+    def get(self, request):
+        if self.request.user:
+            user_login = request.user.email
+            count_of_logout[user_login] = count_of_logout.get(user_login, 0) + 1
+            logout(request)
+            response = redirect('home_page')
+            response.set_cookie('latest_user_login', user_login)
+            response.set_cookie('count_of_logout', count_of_logout[user_login])
+            return response
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    success_url = reverse_lazy('login_page')
+    template_name = 'User/signup.html'
+
