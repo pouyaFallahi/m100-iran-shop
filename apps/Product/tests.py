27d49828c3da033f0aa7d05c3528456c5ce256e6
pouyaFallahi <pouya.fallahi80@gmail.com>
2024-01-17 22:28:41 +0330

    UPDATE | now is better

--- apps/Product/tests.py
+++ apps/Product/tests.py
@@ -1,15 +1,14 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
 from .models import Category, Company, Product
+
 
 class ModelsTestCase(TestCase):
     def setUp(self):
-
         category = Category.objects.create(name_category='Test Category')
         company = Company.objects.create(company_name='Test Company', datil='Test Details', email='test@example.com')
-        product = Product.objects.create(name_product='Test Product', datil='Test Details', price=100, many=10, categotory=category, company=company)
+        product = Product.objects.create(name_product='Test Product', datil='Test Details', price=100, many=10,
+                                         categotory=category, company=company)
+        return category, company, product
 
     def test_category_str(self):
         category = Category.objects.get(name_category='Test Category')
@@ -23,33 +22,18 @@ class ModelsTestCase(TestCase):
         product = Product.objects.get(name_product='Test Product')
         self.assertEqual(str(product), 'Test Product')
 
-
-    #add Products
-    def setUp(self):
-        # ایجاد داده‌های تست
-        category = Category.objects.create(name_category='Test Category')
-        company = Company.objects.create(company_name='Test Company', datil='Test Details', email='test@example.com')
+    # add Products
 
     def test_add_product_to_database(self):
         # ایجاد یک محصول جدید
-        category = Category.objects.get(name_category='Test Category')
-        company = Company.objects.get(company_name='Test Company')
-
-        new_product = Product.objects.create(
-            name_product='New Test Product',
-            datil='Test Details',
-            price=150,
-            many=20,
-            categotory=category,
-            company=company
-        )
+        category, company, product = self.setUp()
 
         # بررسی وجود محصول در دیتابیس
-        self.assertIsNotNone(new_product.id)
+        self.assertIsNotNone(product.id)
 
         # بررسی صحت مقادیر وارد شده
-        self.assertEqual(new_product.name_product, 'New Test Product')
-        self.assertEqual(new_product.price, 150)
-        self.assertEqual(new_product.many, 20)
-        self.assertEqual(new_product.categotory, category)
-        self.assertEqual(new_product.company, company)
+        self.assertEqual(product.name_product, 'Test Product')
+        self.assertEqual(product.price, 100)
+        self.assertEqual(product.many, 10)
+        self.assertEqual(product.categotory, category)
+        self.assertEqual(product.company, company)
