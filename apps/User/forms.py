40f69f888e2bd98ac17eb7e5709fb645f1cd1b1a
pouyaFallahi <pouya.fallahi80@gmail.com>
2024-01-18 00:05:10 +0330

    UPDATE | style updated

--- apps/User/forms.py
+++ apps/User/forms.py
@@ -1,7 +1,15 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
+
+
+class YourForm(forms.Form):
+    your_field = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'aria-label': 'Sizing example input',
+                                      'aria-describedby': 'inputGroup-sizing-default'})
+    )
 
 
 class SignUpForm(UserCreationForm):
