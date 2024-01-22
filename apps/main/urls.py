9a5da59057eaa9d517999a7a397c2da4de987ebb
pouyaFallahi <pouya.fallahi80@gmail.com>
2024-01-22 16:02:38 +0330

    UPDATE | addresses for cookiews have been set, *but there is a bug that goes to another page*

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from .views import homepage
 from ..User.views import LogingView, LogOutView, SignUpView
-from ..Product.views import ShowAllItems, ShowItem, AddToCartView, RemoveFromCartView
+from ..Product.views import ShowAllItems, ShowItem, add_to_cart, remove_from_cart, show_cart_items, remove_all_cart
 
 urlpatterns = [
     path('', homepage, name='home_page'),
@@ -12,6 +12,8 @@ urlpatterns = [
     path('user/signup', SignUpView.as_view(), name='signup_page'),
     path('product/', ShowAllItems.as_view(), name='item_cart'),
     path('product/<int:pk>/', ShowItem.as_view(), name='item_detail'),
-    path('add_to_cart/', AddToCartView.as_view(), name='add_to_cart'),
-    path('remove_from_cart/', RemoveFromCartView.as_view(), name='remove_from_cart'),
+    path('add_to_cart/<int:product_id>/', add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
+    path('product/List_of_orders/', show_cart_items, name='show_cart_items'),
+    path('product/remove_all_orders/', remove_all_cart, name='remove_all'),
 ]
