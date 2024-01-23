8590cad7787a2372b6a71239f7f71c0889b630a2
pouyaFallahi <pouya.fallahi80@gmail.com>
2024-01-23 16:01:41 +0330

    UPDATE | for the convenience of the user and to change the slider photo on the home page, a model was created that can easily change it

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class CarouselImg(models.Model):
+    img_carousel_url = models.ImageField(upload_to='apps/main/images/')
+    img_carousel_title = models.CharField(max_length=256, blank=True, null=True)
+
+    def __str__(self):
+        return self.img_carousel_title
+
