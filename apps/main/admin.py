fe4a385522ca39cbb61913d10485892c32ec3407
pouyaFallahi <pouya.fallahi80@gmail.com>
2024-01-23 15:57:33 +0330

    UPDATE | The main page was changed from base to home-page

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from ..Product.models import Product, Company, Category
 from ..User.models import User
+from .models import CarouselImg
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Product)
 admin.site.register(Company)
 admin.site.register(Category)
+admin.site.register(CarouselImg)
--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
+from .models import CarouselImg
+from ..Product.models import Category
 from django.shortcuts import render
+from django.views.generic import ListView
 
 
-def homepage(request):
-    return render(request, 'base.html')
+def carousel_view(request):
+    carousel_img = CarouselImg.objects.all()
+    category = Category.objects.all()
+    print(category.values())
+    return render(request, 'home-page.html', {'carousel_img': carousel_img, 'category': category})
+
+
