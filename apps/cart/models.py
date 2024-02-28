2a41c7b3566493a41ef2a1352da497443e05ba5a
pouyaFallahi <pouya.fallahi80@gmail.com>
2024-02-28 15:38:49 +0330

    CHANGE | this items name changed

--- apps/cart/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from ..User.models import User
-from ..Product.models import Product
-
-
-class CartItem(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default=1)
